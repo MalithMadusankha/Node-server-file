41ce9a1f2e39e3067a3d3b71384312649d799176
Malith Madusankha <78203677+MalithMadusankha@users.noreply.github.com>
2022-06-07 14:52:22 +0530

    initial commit

---
+++ node-server-file/server.js
@@ -0,0 +1,24 @@
+const express = require("express"); //Import the express dependency
+const app = express(); //Instantiate an express app, the main work horse of this server
+const port = 5000; //Save the port number where your server will be listening
+var fs = require("fs");
+//Idiomatic expression in express to route and respond to a client request
+app
+  .get("/", (req, res) => {
+    res.json("Hello Server");
+  })
+  .on("error", function (err) {
+    // Handle errors
+    fs.unlink(myFile); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+
+app.get("/doc", function (req, res) {
+  const file = `${__dirname}/test.txt`;
+  res.download(file); // Set disposition and send it.
+});
+
+app.listen(port, () => {
+  //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`<< Now Listening On Port ${port} >>`);
+});
